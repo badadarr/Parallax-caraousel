fa4de1760faf746089b2d3abacf6b4a465cd91e0
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-09 20:49:38 +0700

    Thats Good

---
+++ SwiftUI Parallax Carousel/SwiftUI_Parallax_CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Parallax_CarouselApp.swift
+//  SwiftUI Parallax Carousel
+//
+//  Created by badar on 09/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Parallax_CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
