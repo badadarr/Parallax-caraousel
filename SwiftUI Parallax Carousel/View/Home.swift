d56a63d179f7ef83658e50ae3f1bf77789d38161
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-10 05:39:50 +0700

    Update

---
+++ SwiftUI Parallax Carousel/View/Home.swift
@@ -0,0 +1,31 @@
+//
+//  Home.swift
+//  SwiftUI Parallax Carousel
+//
+//  Created by badar on 09/12/20.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var selected : Int = 0
+    var width = UIScreen.main.bounds.width
+    var height = UIScreen.main.bounds.height
+    
+    var body: some View {
+        
+        TabView(selection: $selected) {
+            
+             // Gambar...
+            ForEach(1...6,id: \.self) { index in
+                
+                Image("p\(index)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: width, height: height / 2)
+                    .cornerRadius(15)
+            }
+        }
+    }
+}
+
