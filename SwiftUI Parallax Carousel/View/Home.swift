907317c644140a583c71494dde505ef5ca89598a
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-10 08:39:40 +0700

    いう寿司

--- SwiftUI Parallax Carousel/View/Home.swift
+++ SwiftUI Parallax Carousel/View/Home.swift
@@ -17,13 +17,30 @@ struct Home: View {
         TabView(selection: $selected) {
             
              // Gambar...
-            ForEach(1...6,id: \.self) { index in
+            ForEach(1...4,id: \.self) { index in
                 
-                Image("p\(index)")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: width, height: height / 2)
-                    .cornerRadius(15)
+                // Geometry reader For Parallax Effect...
+                
+                GeometryReader { reader in
+                    
+                    Image("p\(index)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: width, height: height / 2)
+                        .cornerRadius(15)
+                }
+                .frame(height: height / 2)
+                .cornerRadius(15)
+                .padding(10)
+                .background(Color.white)
+                .cornerRadius(15)
+                // shadow..
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: -5, y: -5)
+                // decreasing width by padding...
+                // so outer view only decreased..
+                // inner image will be full width....
+                .padding(.horizontal, 25)
             }
         }
         .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
